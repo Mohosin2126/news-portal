1995ab8370bb62639acbbe05b5bed933cc6f26d3
mohosin2126 <mohosin212@gmail.com>
2023-09-22 15:29:32 +0800

    card design remain

--- script.js
+++ script.js
@@ -17,8 +17,27 @@ data.data.news_category.forEach((category)=>{
 const handleNews=async(categoryId)=>{
     const res=await fetch(`https://openapi.programming-hero.com/api/news/category/${categoryId}`)
 const data=await res.json()
-console.log(data)
-
+const newsCard=document.getElementById("news-card")
+newsCard.textContent=" "
+data.data.forEach((item)=>{
+  const div=document.createElement("div")
+  div.innerHTML=`
+  
+  <div class="card w-96 bg-base-100 shadow-xl">
+  <figure><img src="${item.image_url}" alt="Shoes" /></figure>
+  <div class="card-body">
+    <h2 class="card-title"></h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+  
+  
+  `
+  newsCard.appendChild(div)
+})
 
     }
     
